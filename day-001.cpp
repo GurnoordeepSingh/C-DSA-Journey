ea4e1bcd532093cd2fd35a74d57a9797e3ec6d95
Gurnoordeep Singh <gurnoordeepsingh80@gmail.com>
2026-07-20 00:17:54 +0530

    day 1: cout basics, print, return 0 basics

---
+++ day-001.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello Lucid"<<endl;
+    return 0; //it means that the program has ended successfully and returned 0 to the operating system, else it would return a non-zero value to indicate an error.
+}
